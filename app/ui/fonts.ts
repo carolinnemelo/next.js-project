85a1247156cfbc68ef479694fc1687ee189639f7
carolinnepmelo@gmail.com <carolinnepmelo@gmail.com>
2024-03-28 11:17:05 +0100

    first commit step 5

---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { Inter, Lusitana } from 'next/font/google';
+
+
+const lusitana = Lusitana({weight: '700',  subsets: ['latin']})
+const inter = Inter({ subsets: ['latin']});
+
+export { inter, lusitana }
